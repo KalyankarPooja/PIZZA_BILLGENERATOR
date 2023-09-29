4f27e57690f20bb67c2e64c3097b9eb35d193067
KalyankarPooja <136887565+KalyankarPooja@users.noreply.github.com>
2023-09-29 23:03:37 +0530

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,11 @@
+class Pizza{
+    String crust;
+    String cheese;
+    String toppies;
+    String size;
+    String nonVeg;
+    int quantity;
+}
+public static void main(String args[]){
+    
+}
